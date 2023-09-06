bb53a8fe674c1b001bb8228ee66ba427d75dca98
naruls <76217299+naruls@users.noreply.github.com>
2023-09-06 15:58:53 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -23,7 +23,7 @@ export class Api {
 }
 
 const apiMain = new Api({
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:3001',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
